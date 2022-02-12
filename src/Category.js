42f36c7d4f8fbd7a334a1d9e72d98ac84d8ec61a
ihsansunman <ihsansunman@yandex.com>
2022-02-12 18:03:18 +0300

    Create category event

--- src/Category.js
+++ src/Category.js
@@ -2,27 +2,23 @@ import React, { Component } from "react";
 import { Container, ListGroup, ListGroupItem } from "reactstrap";
 
 export default class Category extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      categories: [
-        { categoryId: 1, categoryName: "Beverages" },
-        { categoryId: 2, categoryName: "Condiments" },
-      ],
-    };
-  }
+  state = {
+    categories: [
+      { categoryId: 1, categoryName: "Beverages" },
+      { categoryId: 2, categoryName: "Condiments" },
+    ],
+    currentCategory: "",
+  };
   render() {
     return (
       <Container>
         <ListGroup>
           <ListGroupItem color="success">{this.props.info.title}</ListGroupItem>
-          {
-            this.state.categories.map(category => (
-              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
-            ))
-          }
-          
+          {this.state.categories.map((category) => (
+            <ListGroupItem onClick={() => this.setState({currentCategory: category.categoryName})} key={category.categoryId}>{category.categoryName}</ListGroupItem>
+          ))}
         </ListGroup>
+        <h5>{this.state.currentCategory}</h5>
       </Container>
     );
   }
