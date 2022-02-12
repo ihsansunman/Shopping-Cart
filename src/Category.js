08a5c54b241cf6669100a5b1a7ce831e1dc3717b
ihsansunman <ihsansunman@yandex.com>
2022-02-12 17:51:20 +0300

    Create Category State

--- src/Category.js
+++ src/Category.js
@@ -1,17 +1,27 @@
 import React, { Component } from "react";
-import { Container,ListGroup, ListGroupItem } from "reactstrap";
+import { Container, ListGroup, ListGroupItem } from "reactstrap";
 
 export default class Category extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      categories: [
+        { categoryId: 1, categoryName: "Beverages" },
+        { categoryId: 2, categoryName: "Condiments" },
+      ],
+    };
+  }
   render() {
     return (
       <Container>
-        <h3>{this.props.info.title}</h3>
         <ListGroup>
-          <ListGroupItem>Cras justo odio</ListGroupItem>
-          <ListGroupItem>Dapibus ac facilisis in</ListGroupItem>
-          <ListGroupItem>Morbi leo risus</ListGroupItem>
-          <ListGroupItem>Porta ac consectetur ac</ListGroupItem>
-          <ListGroupItem>Vestibulum at eros</ListGroupItem>
+          <ListGroupItem color="success">{this.props.info.title}</ListGroupItem>
+          {
+            this.state.categories.map(category => (
+              <ListGroupItem key={category.categoryId}>{category.categoryName}</ListGroupItem>
+            ))
+          }
+          
         </ListGroup>
       </Container>
     );
